1b6a0eb58d43c82085b5daf17c0335ec86b51f5f
Kelvin <kelvin.gamedeveloper@gmail.com>
2015-10-18 14:36:52 -0200

    Teste para verificar o problema de carrregamento da imagem do líder

--- header/main.cpp
+++ header/main.cpp
@@ -2,17 +2,22 @@
 bibliotecas. */
 
 #include <iostream>
+#include <time.h>
 #include <fstream> // E / S de arquivos
 #include <iomanip> // Para ler em caracter a caracter
 #include <graphics.h>
 
+void GetImage(void** pImg, char path[], int width, int height);
+
+
 /*Faça seus testes com os arquivos header aqui (;*/
-#include "../td_functions.h"
 #include "../td_defines.h"
+#include "../tdelay.h"
 #include "../sprite.h"
 #include "../campojogo.h"
 #include "../grade.h"
 #include "../soldado.h"
+
 
 using namespace std;
 
@@ -22,11 +27,36 @@ int main(){
 	// Inicialize a janela gráfica
 	initwindow(TELA_W,TELA_H);
 	
+	void *img;
+	readimagefile("../../Assets/Lider/Stalin/Stalin01.bmp",0,0,LIDER_TILEW,LIDER_TILEH); 
+	int size = imagesize(0,0,LIDER_TILEW,LIDER_TILEH);
+	img = malloc(size);
+	getimage(0,0,LIDER_TILEW,LIDER_TILEH,img); 
+	cleardevice();
+	putimage(0,0,img,0);
+
+	
+
+	
 	while(!kbhit());
 	closegraph();
 	return 0;	
 }
 
-	
+/*Busca uma imagem com as informações passadas por parâmetro*/
+void GetImage(void** pImg, char path[], int width, int height){
+		
+		// Lê e coloca na tela uma imagem
+		readimagefile(path,0,0,width,height); 
+		
+		/// Calcula o tamanho da imagem com base na posição
+		int size = imagesize(0,0,width,height);
+		
+		// Aloca memória para a variável que vai recebe-la
+		*pImg = malloc(size);
+		
+		// Recebe a imagem
+		getimage(0,0,width,height,*pImg); 
+}	
 
 
