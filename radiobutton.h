65a6dd208a0862cb6c0e0fe41709ec9b7c434b78
Kelvin <kelvin.gamedeveloper@gmail.com>
2015-12-14 16:53:12 -0200

    Implementação na struct radio e radiolist concluída. Porém, o problema não foi resolvido

--- radiobutton.h
+++ radiobutton.h
@@ -1,15 +1,11 @@
 	
 // Radiobutton single component (similar to the used on Web)
-class Radio{
-	
-	private:	
-		static const int RADIUS = 10;
-		int x,y;
-		bool checked;
-		char *label;
-	
-	public:
-		Radio(char *label, bool checked, int myX, int myY);
+struct Radio{
+
+	int x,y;
+	bool checked;
+	char *label;
+	Radio(char *label, bool checked, int myX, int myY);
 };
 	
 // Radio constructor
@@ -19,53 +15,4 @@ Radio::Radio(char *label1, bool checked1, int myX, int myY){
 	checked = checked1;
 	label = label1;
 }
-
-
-
-	
-/*
-	
-	//Show the list of radiobuttons
-	void Radio::ShowList(Radio *radio0){
-		Radio *pRadio = NULL;
-		
-		setcolor(LIGHTGREEN);
-		setfillstyle(1,LIGHTGREEN);
-	
-		for(pRadio = radio0->next; pRadio != NULL; pRadio = pRadio->next){
-			
-			outtextxy(pRadio->x + 15, pRadio->y + 5, pRadio->label);
-			if(pRadio->checked == false)
-				circle(pRadio->x,pRadio->y,RADIUS);
-			else
-				fillellipse(pRadio->x,pRadio->y,RADIUS,RADIUS);
-		}
-	}
-	
-	
-	// Check for possible clicks in the radiobuttons list
-	void Radio::CheckClick(Radio *radio0){
-		
-		Radio *pRadio = NULL;
-		int mouseX,mouseY;
-		double tempX, tempY, distRadius;
-		bool checkRadio = false;
-	
-		if(GetKeyState(VK_LBUTTON) & 0x80){
-			
-			mouseX = mousex();
-			mouseY = mousey();
-			for(pRadio = radio0->next; pRadio != NULL; pRadio = pRadio->next){
-				
-				tempX = pow(pRadio->x - mouseX,2.0);
-				tempY = pow(pRadio->y - mouseY,2.0);
-				distRadius = sqrt(tempX + tempY);
-				
-				if(distRadius <= RADIUS){
-					pRadio->CheckRadio(radio0);		
-				}
-			}
-		}
-	}
-*/
 
--- radiolist.h
+++ radiolist.h
@@ -1,13 +1,80 @@
 // Radiobutton set (similar to the used on Web)
-class RadioList
+struct RadioList
 {	
-	private:
-		std::vector<Radio> list;
-		
-	public:
-		Radio* RadioChecked();
-		bool CheckRadio(char* label);
+	// Proprieties	
+	static const int RADIUS = 10;
+	std::vector<Radio> list;
+	
+	// Methods
+	void CheckRadio(Radio *pRadio);	
+	Radio* RadioChecked();
+	RadioList();
+	RadioList(std::vector<Radio> list);
+	ShowList();
+	void CheckClick();
+	
+};
+
+// Construtor without paramters
+RadioList::RadioList()
+{
+	
 }
+
+// Constructor that receives Radio's in one vector
+RadioList::RadioList(std::vector<Radio> list)
+{
+	this->list = list;
+}
+
+// Turn On the selected radio and Turn Off the other that is setted
+void RadioList::CheckRadio(Radio *pRadio)
+{	
+	int i;
+	Radio *current;
+
+	if(pRadio->checked == false)
+	{
+		current = RadioChecked();
+		if(current != NULL)
+		{
+			current->checked = false;
+		}
+				
+		pRadio->checked = true;
+	}
+		
+}
+
+	
+// Check for possible clicks in the radiobuttons list
+void RadioList::CheckClick()
+{
+	
+	int mouseX, mouseY, i;
+	double distRaidus, tempX, tempY;
+	
+	if(GetKeyState(VK_LBUTTON) & 0x80)
+	{
+		mouseX = mousex();
+		mouseY = mousey();
+		
+		for(i = 0; i < list.size(); i++)
+		{
+			tempX = pow(list[i].x - mouseX, 2.0);
+			tempY = pow(list[i].y - mouseY,2.0);
+			distRaidus = sqrt(tempX + tempY);
+			
+			if(distRaidus <= RADIUS)
+			{
+				CheckRadio(&list[i]);
+			}
+		}
+			
+	}
+	
+}
+
 
 // Returns the radio that was selected
 Radio* RadioList::RadioChecked()
@@ -26,54 +93,27 @@ Radio* RadioList::RadioChecked()
 	return pRadio;
 }
 
-// Turn On the selected radio and Turn Off the other that is setted
-bool Radio::CheckRadio(char* label)
-{	
+
+
+
+//Show the list of radiobuttons
+RadioList::ShowList()
+{
 	int i;
-	Radio *pRadio = NULL;
-	Radio *currentCheck = NULL;
+	setcolor(LIGHTGREEN);
+	setfillstyle(1,LIGHTGREEN);
 	
 	for(i = 0; i < list.size(); i++)
 	{
-		if(list[i].label == label)
-		{
-			break;
-		}
-		
+		outtextxy(list[i].x + 15, list[i].y + 5, list[i].label);
+		if(list[i].checked == false)
+			circle(list[i].x,list[i].y,RADIUS);
+		else
+			fillellipse(list[i].x,list[i].y,RADIUS,RADIUS);
 	}
-	
-	if(pRadio != NULL)
-	{
-
-		currentChecked = RadioChecked();
-		if(currentChecked != pRadio)
-		{
-			currentChecked->checked = false;
-			pRadio->checked = true;
-		}
-		
-		
-		return true;
-	} else
-	{
-		return false;
-	}
-	
-	
-	
-	
 }
+	
 
 
 
-/*
-// Turn On the selected radio and Turn Off the others
-	void Radio::CheckRadio(Radio *radio0){
-		
-		Radio *pRadio = NULL;
-		for(pRadio = radio0->next; pRadio != NULL; pRadio = pRadio->next ){
-			pRadio->checked = false;	
-		}
-		this->checked = true;
-	}
-*/	
+
